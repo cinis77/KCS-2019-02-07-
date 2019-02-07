e90f53fd54678094283a97385e47ca665c0068de
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-02-07 19:28:44 +0200

    UZduotis 1

---
+++ ProgramaKintamieji/ProgramaKintamieji/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaKintamieji
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string Vardas = "Dominykas";
+            string Pavarde = "Rentelis";
+            int Amzius = 26;
+            double ugis = 1.8;
+            double svoris = 96;
+            string AukstojiMokykla = "Kauno Technologiju Universitetas";
+            string AkademineGrupe = "RB @$ 1/4";
+            char Kursas = '2';
+            string Studiju_Programa = "Informacines technologijos";
+            int atsiskaitytaKreditu = 180;
+        }
+    }
+}

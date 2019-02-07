a46d689c6cf7264919f97f5379b962f0ce1f01a1
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-02-07 21:17:03 +0200

    Skaiciavimai

---
+++ AritmetiniaiVeiksmai/AritmetiniaiVeiksmai/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AritmetiniaiVeiksmai
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = a + b;
+            int skirtumas = a - b;
+            int sandauga = a * b;
+            double dalmuo = (double)a / b;
+            Console.WriteLine("{0} + {1} = {2}", a, b, sum);
+            Console.WriteLine("{0} - {1} = {2}", a, b, skirtumas);
+            Console.WriteLine("{0} * {1} = {2}", a, b, sandauga.ToString("F2"));
+            Console.WriteLine("{0} / {1} = {2:0.00}", a, b, dalmuo);
+        }
+    }
+}

cef1b3e65a22bab2057782f743f6a262427f4712
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-02-07 19:15:22 +0200

    Pirma programa

--- PirmasProjektas/PirmasProjektas/Program.cs
+++ PirmasProjektas/PirmasProjektas/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace PirmasProjektas
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
+
         {
+            //Pascal => PRasideda didziaja raide Globaliems kintamiesies (Globalus klaseje)
+            //camel => Prasideda mazaja, Didziaja kitas zodis Lokalieji metodui
+
+            int Skaicius; // Skaicius = 0
+            Skaicius = 3;
+            float SkaiciusSuKableliu = 8.56f;
+            double Skaicius2 = 8.56;
+
+            char raide; //Null
+            raide = 'n';
+            //raide = "n"; => negalimas variantas
+            string zodis;
+            zodis = "Zodis   Zodis";
+            bool tiesaNetiesa;
+            tiesaNetiesa = true;
+            tiesaNetiesa = false;
+
+            var kinamasis = "";
+            //kinamasis = 4.5;
+            var _kintamasis = 4;
+            // var 8fd = 45;
+            // var dsdkdf#$%^%^& = 45;
+            int Kintamasis1223 = 12;
         }
     }
 }

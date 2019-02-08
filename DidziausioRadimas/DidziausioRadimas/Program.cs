f4c04a6941cf7f98537c0bbe50f65e366c407247
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-02-08 21:21:00 +0200

    .

---
+++ DidziausioRadimas/DidziausioRadimas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DidziausioRadimas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk3 = Convert.ToInt32(Console.ReadLine());
+            if (sk1 > sk2 && sk1 > sk3)
+            {
+                Console.WriteLine("Skaicius {0} didziausias", sk1);
+            }
+            else if (sk2 > sk1 && sk2 > sk3)
+            {
+                Console.WriteLine("Skaicius {0} didziausias", sk2);
+            }
+            else if (sk3 > sk1 && sk3 > sk2)
+            {
+                Console.WriteLine("Skaicius {0} didziausias", sk3);
+            }
+        }
+    }
+}
---
+++ TrikampiuPaieska/TrikampiuPaieska/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrikampiuPaieska
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk3 = Convert.ToInt32(Console.ReadLine());
+            if ((sk1 + sk2) > sk3)
+            {
+                if ((sk2 + sk3) > sk1)
+                {
+                    if ((sk1 + sk3) > sk2)
+                    {
+                        Console.WriteLine("Trikampi sudarytis gamlima");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Trikampio sudaryti negalima");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Trikampio sudaryti negalima");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Trikampio sudaryti negalima");
+            }
+
+            if ((sk2 + sk3) > sk1 && (sk1 + sk2) > sk3 && (sk1 + sk3) > sk2)
+            {
+                Console.WriteLine("Trikampi sudarytis gamlima");
+            }
+        }
+    }
+}

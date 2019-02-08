9e2fb77f990af773e7303c522d963a7ff71b04c6
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-02-08 18:53:58 +0200

    Antros dienos klases uzduotis

---
+++ AritmetikaAntraDiena/AritmetikaAntraDiena/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AritmetikaAntraDiena
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dalybos atsakymas: " + ((double)sk1 / sk2));
+            Console.WriteLine("Sveikoji dalis: " + (sk1 / sk2));
+            Console.WriteLine("Liekana yra: " + (sk1 % sk2));
+        }
+    }
+}

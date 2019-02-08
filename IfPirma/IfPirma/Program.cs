0319c3839fa816155ad80674001fb41d3940a4d2
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-02-08 19:20:35 +0200

    Uzduotys su If

---
+++ IfPirma/IfPirma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfPirma
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+            if (sk1 > sk2)
+            { //jei salyga tenkina vykdo koda
+                Console.WriteLine("Pirmas skaicius {0} yra didesnis uz "
+                    + "antra skaiciu {1}", sk1, sk2);
+            }
+            if (sk1 < sk2)
+            {
+                Console.WriteLine("Pirmas skaicius {0} yra mazesnis uz "
+                    + "antra skaiciu {1}", sk1, sk2);
+            }
+            if (sk1 == sk2)
+            {
+                Console.WriteLine("Äbu skaiciai yra lygus");
+            }
+        }
+    }
+}
---
+++ LyginioPatikrinimas/LyginioPatikrinimas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyginioPatikrinimas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            int liekana = sk % 2;
+            if (liekana == 0)
+            {
+                Console.WriteLine("Skaicius yra lyginis");
+            }
+        }
+    }
+}

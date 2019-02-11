364c05eb27a3ab7f42e50b970c2a00b3e8c06ed0
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-02-11 21:20:34 +0200

    `.

---
+++ Aparatas/Aparatas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aparatas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Pasirinkite viena is galimu variantu: ");
+            Console.WriteLine("1-limonadas\n2-arbata\n3-kakava\n4-kava\n5-nieko");
+            Console.WriteLine("Jusu pasirinkimas");
+            string pasirinkinimas = Console.ReadLine();
+            switch (pasirinkinimas)
+            {
+                case "1"://1 => int
+                    Console.WriteLine("Jus pasirinkote limonada");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Jus pasirinkote arbata");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Jus pasirinkote kakava");
+                    break;
+
+                case "4":
+                    Console.WriteLine("Jus pasirinkote kava");
+                    break;
+
+                case "5":
+                    Console.WriteLine("Jus pasirinkote nieko");
+                    break;
+
+                default:
+                    Console.WriteLine("Blogas pasirinkimas");
+                    break;
+            }
+        }
+    }
+}
---
+++ SwitchPirma/SwitchPirma/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchPirma
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk3 = Convert.ToInt32(Console.ReadLine());
+            switch (sk1)
+            {
+                case 1:
+                    Console.WriteLine("Suma :" + (sk1 + sk2 + sk3));
+                    break;
+
+                case 2:
+                    Console.WriteLine("Skirtumas :" + (sk1 - sk2));
+                    break;
+
+                case 3:
+                    Console.WriteLine("Sandauga :" + (sk2 * sk3));
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}

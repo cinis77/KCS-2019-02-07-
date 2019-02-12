1845ffacb500a1dcc85f5de593b6298f78840295
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-02-12 21:19:45 +0200

    Uzduotys klasėje

---
+++ FibonacioSkaiciai/FibonacioSkaiciai/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacioSkaiciai
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Kiek norite matyti fibonacio skaiciu?");
+            int kiekis = Convert.ToInt32(Console.ReadLine());
+            int sk1 = 1;
+            int sk2 = 1;
+            int sk3 = 0;
+            Console.Write(sk1 + " " + sk2);
+            for (int i = 2; i < kiekis; i++)
+            {
+                sk3 = sk1 + sk2;
+                Console.Write(" " + sk3);
+                sk1 = sk2;
+                sk2 = sk3;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ MiegoSkaiciavimas/MiegoSkaiciavimas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiegoSkaiciavimas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //59
+            for (int a = 0; a <= 18; a++)
+            {
+                double t = 8 + (double)(18 - a) / 2; // => valandos pramiegotos per diena
+                t *= 365;
+                Console.WriteLine(t);
+            }
+            //60
+            for (int i = -10; i <= 10; i++)
+            {
+                double ats = Math.Pow(i, 2) * 7 + 5 * i - 3;
+                Console.WriteLine(ats);
+            }
+            //61
+            for (int i = 0; i < DateTime.Today.Year; i++)
+            {
+                if ((i % 4) == 0)
+                {
+                    if ((i % 100) == 0)
+                    {
+                        if ((i % 400) == 0)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}

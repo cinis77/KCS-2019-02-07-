e48f8d0e96d89e2ff0015c0ee8c0b924b12c0dc6
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-02-12 19:36:51 +0200

    For ciklai

---
+++ PirminiaiSkaiciai/PirminiaiSkaiciai/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirminiaiSkaiciai
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int suma = 0;
+            for (int i = 2; i < 1000; i++)
+            {
+                bool Pirminis = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if ((i % j) == 0)
+                    {
+                        Pirminis = false;
+                        break;
+                    }
+                }
+                if (Pirminis == true) // If(Pirminis == true)
+                {
+                    Console.WriteLine(i);
+                    suma += i;
+                }
+            }
+            Console.WriteLine(suma);
+        }
+    }
+}

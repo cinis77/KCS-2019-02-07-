62ff65da8812e255c6af30473c1e584aef84caa9
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-02-14 19:20:58 +0200

    Ref ir out metodai

---
+++ MetodaiRef/MetodaiRef/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodaiRef
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int sk1 = 10;
+            int sk2 = 5;
+            Console.WriteLine("Skacius1 = {0} Skaicius2 = {1}", sk1, sk2);
+            MEtodasSuRef(sk1, ref sk2);
+            Console.WriteLine("Skacius1 = {0} Skaicius2 = {1}", sk1, sk2);
+        }
+
+        public static void MEtodasSuRef(int a, ref int b)
+        {
+            b = a;
+        }
+    }
+}
---
+++ MetodasOut/MetodasOut/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodasOut
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int sk1 = 10;
+            int sk2 = 15;
+            int sum = 0;
+            double vid = 0;
+            Console.WriteLine("Suma = {0}, vidurkus = {1}", sum, vid);
+            Skaiciavimai(sk1, sk2, out sum, out vid);
+            Console.WriteLine("Suma = {0}, vidurkus = {1}", sum, vid);
+        }
+
+        public static void Skaiciavimai(int a, int b, out int suma, out double vidurkis)
+        {
+            suma = a + b;
+            vidurkis = (double)(a + b) / 2;
+        }
+    }
+}

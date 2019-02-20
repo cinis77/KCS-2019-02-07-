cf2123da828dd2c776dfab689737d55020fd5d48
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-02-20 08:40:00 +0200

    Uzdaviniai per mokymus

---
+++ Aplikacija/Aplikacija/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacija
+{
+    internal class Taskas
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Taskas(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double DistanceBetweenPoints(Taskas taskas)
+        {
+            var distance = Math.Sqrt(Math.Pow(X - taskas.X, 2) + Math.Pow(Y - taskas.Y, 2));
+            return distance;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rng = new Random();
+            List<Taskas> taskai = new List<Taskas>();
+            for (int i = 0; i < rng.Next(10, 50000); i++)
+            {
+                Taskas temp = new Taskas(rng.Next(-100, 100), rng.Next(-100, 100));
+                taskai.Add(temp);
+            }
+
+            Taskas centroTaska = new Taskas(5, 6);
+            double R = 25;
+            foreach (var item in taskai)
+            {
+                if (R >= item.DistanceBetweenPoints(centroTaska))
+                {
+                    Console.WriteLine("x: {0}, y: {1}", item.X, item.Y);
+                }
+            }
+        }
+    }
+}
---
+++ SkaičiųSurikiavimas/SkaičiųSurikiavimas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkaičiųSurikiavimas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rng = new Random();
+            List<int> skaiciai = new List<int>();
+            for (int i = 0; i < rng.Next(0, 90000); i++)
+            {
+                skaiciai.Add(rng.Next(-500, 500));
+            }
+            List<int> SurikiuotiSkaiciai = new List<int>();
+            while (skaiciai.Count != 0)
+            {
+                SurikiuotiSkaiciai.Add(skaiciai.Min());
+                skaiciai.Remove(skaiciai.Min());
+            }
+            foreach (var item in SurikiuotiSkaiciai)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
